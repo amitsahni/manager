ac6cd1490bf1d61a37291d06c87467cde37a650c
Amit <amit@clickapps.co>
2017-08-31 16:45:12 +0530

    Add Demo app

---
+++ app/src/main/java/test/activity/SecondActivity.java
@@ -0,0 +1,40 @@
+package test.activity;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.activityutil.activity.activity.ActivityManager;
+import com.activityutil.activity.activity.ActivityParam;
+import com.activityutil.activity.activity.BaseAppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by clickapps on 31/8/17.
+ */
+
+public class SecondActivity extends BaseAppCompatActivity {
+    @BindView(R.id.button)
+    Button getBtn;
+
+
+    @Override
+    protected void initUI() {
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button) {
+            ActivityManager.with(this, ActivityParam.ActivityType.FINISH)
+                    .build();
+        } else {
+            Log.i(getLocalClassName(), "No clickHandled");
+        }
+    }
+}
