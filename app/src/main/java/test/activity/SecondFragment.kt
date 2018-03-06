0f84707afac91cfad355c527df57f39f107877e0
Amit <amit@clickapps.co>
2018-03-06 16:48:07 +0530

    Change SecondFragment to kotlin

---
+++ app/src/main/java/test/activity/SecondFragment.kt
@@ -0,0 +1,25 @@
+package test.activity
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import com.fragment.BaseFragment
+
+/**
+ * Created by amit on 14/2/18.
+ */
+
+class SecondFragment : BaseFragment() {
+    internal var view: View? = null
+
+    override fun initUI(inflater: LayoutInflater, container: ViewGroup?): View {
+        if (view == null) {
+            view = LayoutInflater.from(activity).inflate(R.layout.activity_second, null)
+            ButterKnife.bind(this, view!!)
+        }
+        return view!!
+    }
+
+
+}
